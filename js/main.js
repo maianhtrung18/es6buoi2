6e17c0af5ad3f502d6e0ecb44ce509bd91c1ad68
TrungMai <anh.maitrung@gmail.com>
2022-12-07 00:23:36 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,150 @@
+let dataGlasses = [
+    {
+        id: "G1",
+        src: "./img/g1.jpg",
+        virtualImg: "./img/v1.png",
+        brand: "Armani Exchange",
+        name: "Bamboo wood",
+        color: "Brown",
+        price: 150,
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ea voluptates officiis? ",
+    },
+    {
+        id: "G2",
+        src: "./img/g2.jpg",
+        virtualImg: "./img/v2.png",
+        brand: "Arnette",
+        name: "American flag",
+        color: "American flag",
+        price: 150,
+        description:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. In assumenda earum eaque doloremque, tempore distinctio.",
+    },
+    {
+        id: "G3",
+        src: "./img/g3.jpg",
+        virtualImg: "./img/v3.png",
+        brand: "Burberry",
+        name: "Belt of Hippolyte",
+        color: "Blue",
+        price: 100,
+        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+    },
+    {
+        id: "G4",
+        src: "./img/g4.jpg",
+        virtualImg: "./img/v4.png",
+        brand: "Coarch",
+        name: "Cretan Bull",
+        color: "Red",
+        price: 100,
+        description: "In assumenda earum eaque doloremque, tempore distinctio.",
+    },
+    {
+        id: "G5",
+        src: "./img/g5.jpg",
+        virtualImg: "./img/v5.png",
+        brand: "D&G",
+        name: "JOYRIDE",
+        color: "Gold",
+        price: 180,
+        description:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error odio minima sit labore optio officia?",
+    },
+    {
+        id: "G6",
+        src: "./img/g6.jpg",
+        virtualImg: "./img/v6.png",
+        brand: "Polo",
+        name: "NATTY ICE",
+        color: "Blue, White",
+        price: 120,
+        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+    },
+    {
+        id: "G7",
+        src: "./img/g7.jpg",
+        virtualImg: "./img/v7.png",
+        brand: "Ralph",
+        name: "TORTOISE",
+        color: "Black, Yellow",
+        price: 120,
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sint nobis incidunt non voluptate quibusdam.",
+    },
+    {
+        id: "G8",
+        src: "./img/g8.jpg",
+        virtualImg: "./img/v8.png",
+        brand: "Polo",
+        name: "NATTY ICE",
+        color: "Red, Black",
+        price: 120,
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, unde enim.",
+    },
+    {
+        id: "G9",
+        src: "./img/g9.jpg",
+        virtualImg: "./img/v9.png",
+        brand: "Coarch",
+        name: "MIDNIGHT VIXEN REMIX",
+        color: "Blue, Black",
+        price: 120,
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit consequatur soluta ad aut laborum amet.",
+    },
+];
+
+let showGlasses = () => {
+    let content = "";
+    let glasses = dataGlasses.map((glass) => {
+        return `
+            <div class="col-4">
+                <div class="vglasses__items">
+                    <img src="${glass.src}" id="${glass.id}" alt="">
+                </div>
+            </div>
+        `;
+    });
+
+    content = glasses.join("");
+    document.querySelector("#vglassesList").innerHTML = content;
+};
+
+showGlasses();
+
+let addClickListener = () => {
+    let glassItems = document.querySelectorAll(".vglasses__items");
+    for (const glassItem of glassItems) {
+        glassItem.addEventListener("click", wearGlass);
+    }
+};
+
+let wearGlass = (glass) => {
+    let data = dataGlasses.find((dataGlass) => {
+        return dataGlass.id === glass.target.id;
+    });
+    document.querySelector(
+        "#avatar"
+    ).innerHTML = `<img src="${data.virtualImg}" alt="">`;
+    
+    showGlassInfo(data)
+};
+
+let showGlassInfo = (glassData) => {
+    let content = `
+        <p class="vglasses__infoTitle">${(glassData.name).toUpperCase()} - ${glassData.brand} (${glassData.color})</p>
+        <div class="glassPrice">
+            <span class="price">$${glassData.price}</span> <span class="status">Stocking</span>
+        </div>
+        <div class="glassesInfoContent">
+            ${glassData.description}
+        </div>
+    `;
+    document.querySelector(".vglasses__info").style.display = "block";
+    document.getElementById("glassesInfo").innerHTML = content;
+};
+
+addClickListener();
